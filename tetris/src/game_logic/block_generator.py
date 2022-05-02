db33bd7ccafab11c2a3d26f5f79b2df60461e5d6
- <>
2022-05-02 20:12:12 +0300

    docstrings

--- tetris/src/game_logic/block_generator.py
+++ tetris/src/game_logic/block_generator.py
@@ -2,6 +2,9 @@ import random
 
 
 class BlockGenerator:
+    """Uusien palikoiden luomisesta huolehtiva luokka.
+    """
+
     def __init__(self, playfield, blocks):
         self._playfield = playfield
         self._blocks = blocks
@@ -17,20 +20,27 @@ class BlockGenerator:
         self._move_to_next_block_area()
 
     def create_random_block(self):
-
+        """Valitsee satunnaisen palikan saatavilla olevista palikoista ja luo sen.
+        """
         block = random.choice(self._blocks)
         self._playfield.initialize_sprites(block[0], block[1])
 
     def _move_to_next_block_area(self):
+        """Liikuttaa next_blockin seuraavalle palikalle tarkoitetulle alueelle.
+        """
         self._playfield.move_group(
             self._playfield.next_block, self.delta_x, self.delta_y)
 
     def _move_to_spawnpoint(self):
+        """Liikuttaa next_blockin seuraavalle palikalle tarkoitetulta alueelta pelialueelle.
+        """
         # moving block without collision checks
         for block_sprite in self._playfield.next_block:
             self._playfield.move_block(
                 block_sprite, -self.delta_x, -self.delta_y)
 
     def _move_from_next_block_to_active(self):
+        """Siirtää next_block sprite groupin active_block sprite grouppiin.
+        """
         self._playfield.active_block.add(self._playfield.next_block)
         self._playfield.next_block.empty()
